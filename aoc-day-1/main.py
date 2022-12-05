3c0d320a530f80d6d4d511f960f1e477b517d257
Katie Snell <ksnell184+github@gmail.com>
2022-12-05 23:27:48 +0000

    Day 1 Part 2 solution

--- aoc-day-1/main.py
+++ aoc-day-1/main.py
@@ -15,5 +15,14 @@ def part1():
     print(elf_calories_array[0])
 
 
+def part2():
+    elf_calories_array.sort(reverse=True)
+    sum_of_top_3 = elf_calories_array[0] + elf_calories_array[1] + elf_calories_array[2]
+    print(sum_of_top_3)
+
+
 # 72017
 part1()
+
+# 212520
+part2()
