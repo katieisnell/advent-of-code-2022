f623eacf534e5c64497a082407e9c4f4a80bb19e
Katie Snell <ksnell184+github@gmail.com>
2022-12-05 23:42:16 +0000

    Day 3 Part 2 solution

--- aoc-day-3/main.py
+++ aoc-day-3/main.py
@@ -27,5 +27,35 @@ def part1():
     print(sum_of_priorities)
 
 
+# Find the item type that corresponds to the badges of each three-Elf group.
+# What is the sum of the priorities of those item types?
+def part2():
+    sum_of_priorities = 0
+    
+    i = 0
+    no_of_elves = len(all_items)
+    
+    while i < no_of_elves:
+        first_elf = all_items[i]
+        second_elf = all_items[i + 1]
+        third_elf = all_items[i + 2]
+        
+        first_elf_set = set(first_elf)
+        second_elf_set = set(second_elf)
+        third_elf_set = set(third_elf)
+        
+        intersection = first_elf_set & second_elf_set & third_elf_set
+        
+        for item in intersection:
+            sum_of_priorities += get_item_priority(item)
+        
+        i += 3
+    
+    print(sum_of_priorities)
+
+
 # 7795
 part1()
+
+# 2703
+part2()
