6544bfe9041ebca7403c3f24e6239d4f4e97600a
Katie Snell <ksnell184+github@gmail.com>
2022-12-05 23:57:51 +0000

    Day 5 Part 2 solution

--- aoc-day-5/main.py
+++ aoc-day-5/main.py
@@ -44,5 +44,50 @@ def part1():
     print(top_of_crates)
 
 
+# After the rearrangement procedure completes, what crate ends up on top of each stack?
+def part2():
+    crate1 = ['Z', 'P', 'M', 'H', 'R']
+    crate2 = ['P', 'C', 'J', 'B']
+    crate3 = ['S', 'N', 'H', 'G', 'L', 'C', 'D']
+    crate4 = ['F', 'T', 'M', 'D', 'Q', 'S', 'R', 'L']
+    crate5 = ['F', 'S', 'P', 'Q', 'B', 'T', 'Z', 'M']
+    crate6 = ['T', 'F', 'S', 'Z', 'B', 'G']
+    crate7 = ['N', 'R', 'V']
+    crate8 = ['P', 'G', 'L', 'T', 'D', 'V', 'C', 'M']
+    crate9 = ['W', 'Q', 'N', 'J', 'F', 'M', 'L']
+    crates = np.array([crate1, crate2, crate3, crate4, crate5, crate6, crate7, crate8, crate9])
+    
+    for step in all_items:
+        move_no_match = re.search('move (\d+)', step)
+        move_no = int(move_no_match.group(1))
+        from_stack_match = re.search('from (\d+)', step)
+        from_no = int(from_stack_match.group(1))
+        to_stack_match = re.search('to (\d+)', step)
+        to_no = int(to_stack_match.group(1))
+        
+        from_index = from_no - 1
+        to_index = to_no - 1
+        current_move_no = move_no
+        items_to_move = []
+        
+        while current_move_no >= 1:
+            popped_item = crates[from_index].pop()
+            items_to_move.append(popped_item)
+            current_move_no = current_move_no - 1
+        
+        items_to_move.reverse()
+        for item in items_to_move:
+            crates[to_index].append(item)
+    
+    top_of_crates = ''
+    for crate in crates:
+        top_of_crates = top_of_crates + crate.pop()
+    
+    print(top_of_crates)
+
+
 # VQZNJMWTR
 part1()
+
+# NLCDCLVMQ
+part2()
